c8aec1b40e0f3d1c578fa1b3146c276570220834
Andrey Zykov <zyka9569@gmail.com>
2025-03-09 09:48:33 +0500

    Add files via upload

---
+++ KUPI SLONA.py
@@ -0,0 +1,5 @@
+user_name = input("Как тебя зовут? ")
+print(user_name + ",", "купи слона!")
+while True:
+    user_answer = input("")
+    print("Все говорят", user_answer + ",", "а ты купи слона!")
